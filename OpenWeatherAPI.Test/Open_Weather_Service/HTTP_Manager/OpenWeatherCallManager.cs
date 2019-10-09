7089c077fce79371ef2a2235c3ba73c0279513d5
Seng Fai Chan <SChan@spartaglobal.com>
2019-10-09 09:24:41 +0100

    setting up DTO

---
+++ OpenWeatherAPI.Test/Open_Weather_Service/HTTP_Manager/OpenWeatherCallManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using RestSharp;
+
+namespace OpenWeatherAPI.Test.Open_Weather_Service.HTTP_Manager
+{
+    class OpenWeatherCallManager
+    {
+        readonly IRestClient client;
+        public OpenWeatherCallManager()
+        {
+            client = new RestClient(OpenWeatherConfig.BaseUrl);
+        }
+
+        public string GetWeatherForecast()
+        {
+            RestRequest request = new RestRequest("/something" + OpenWeatherConfig.ApiUrlMod + OpenWeatherConfig.ApiKey);
+            IRestResponse response = client.Execute(request);
+            return response.Content;
+        }
+
+    }
+}
--- OpenWeatherAPI.Test/Open_Weather_Service/OpenWeatherService.cs
+++ OpenWeatherAPI.Test/Open_Weather_Service/OpenWeatherService.cs
@@ -3,20 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenWeatherAPI.Test.Open_Weather_Service.Data_Handling;
+using OpenWeatherAPI.Test.Open_Weather_Service.HTTP_Manager;
+using Newtonsoft.Json.Linq;
+
 
 namespace OpenWeatherAPI.Test.Open_Weather_Service
 {
     class OpenWeatherService
     {
         //DTO
-
+        OpenWeatherDTO openWeatherDTO = new OpenWeatherDTO();
         //Call Manager
-
+        OpenWeatherCallManager openWeatherCallManager = new OpenWeatherCallManager();
         //JObject
-
+        JObject weatherResultJson;
         public OpenWeatherService()
         {
-
+            openWeatherDTO.DeserializeWeatherForecast(openWeatherCallManager.GetWeatherForecast());
         }
 
 
